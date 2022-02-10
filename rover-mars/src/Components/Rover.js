1579e2b52a11cc9127b3ecfa350fcd3f8b28519c
Jaume Cordoba <jaumecordoba@mbp-de-jaume.home>
2022-02-10 10:03:47 +0100

    obstacle detector wip

--- rover-mars/src/Components/Rover.js
+++ rover-mars/src/Components/Rover.js
@@ -2,14 +2,34 @@ import React, { useState } from 'react';
 import './Rover.css';
 
 export const Rover = () => {
+
     let [x, setX] = useState(0);
     let [y, setY] = useState(0);
+    let currentPosition = { x, y }
+    let statusObstacle = false;
     const [facing, setFacing] = useState('North');
 
-    const obstacle = { x: 1, y: 0 };
+    const obstacles = {
+        obstacle1: { x: 1, y: 0 },
+        obstacle2: { x: 1, y: 1 },
+    }
 
+
+    const obstacleDetector = () => {
+
+        console.log(currentPosition);
+        if (currentPosition == obstacles.obstacle1 || obstacles.obstacle2) {
+            console.log('Obstacle in front');
+            statusObstacle = true
+        }
+        if (statusObstacle == true) {
+
+        }
+    }
+    console.log(statusObstacle);
 
     const goForward = () => {
+        obstacleDetector()
         if (facing == 'North') {
             if (y == 200) {
                 console.log('moving rover to an invalid position');
@@ -19,9 +39,8 @@ export const Rover = () => {
                 setY(y + 1)
                 setFacing('North')
             }
-        }
-        else if (facing == 'East') {
-            if (x == 0) {
+        } else if (facing == 'East') {
+            if (x == 200) {
                 console.log('moving rover to an invalid position');
                 return
             }
@@ -29,8 +48,7 @@ export const Rover = () => {
                 setX(x + 1)
                 setFacing('East')
             }
-        }
-        else if (facing == 'West') {
+        } else if (facing == 'West') {
             if (x == 0) {
                 console.log('moving rover to an invalid position');
                 return
@@ -39,8 +57,7 @@ export const Rover = () => {
                 setX(x - 1)
                 setFacing('West')
             }
-        }
-        else if (facing == 'South') {
+        } else if (facing == 'South') {
             if (y == 0) {
                 console.log('moving rover to an invalid position');
                 return
@@ -55,28 +72,24 @@ export const Rover = () => {
         }
     }
 
-
     const rotateRight = () => {
         if (facing == 'North') {
             setFacing('East')
             console.log('Rover rotating to East');
-        }
-        else if (facing == 'East') {
+
+        } else if (facing == 'East') {
             setFacing('South')
             console.log('Rover rotating to South');
 
-        }
-        else if (facing == 'West') {
+        } else if (facing == 'West') {
             setFacing('North')
             console.log('Rover rotating to Nort');
 
-        }
-        else if (facing == 'South') {
+        } else if (facing == 'South') {
             setFacing('West')
             console.log('Rover rotating to West');
 
-        }
-        else {
+        } else {
             console.log('moving rover to an invalid position');
         }
     }
@@ -86,18 +99,15 @@ export const Rover = () => {
         if (facing == 'North') {
             setFacing('West')
             console.log('Rover rotating to West');
-        }
-        else if (facing == 'East') {
+        } else if (facing == 'East') {
             setFacing('North')
             console.log('Rover rotating to Nort');
 
-        }
-        else if (facing == 'West') {
+        } else if (facing == 'West') {
             setFacing('South')
             console.log('Rover rotating to South');
 
-        }
-        else if (facing == 'South') {
+        } else if (facing == 'South') {
             setFacing('East')
             console.log('Rover rotating to East');
         }
@@ -105,17 +115,18 @@ export const Rover = () => {
             console.log('moving rover to an invalid position');
         }
     }
-    console.log('x:', x, 'y:', y, facing);
+
+    console.log('x:', currentPosition.x, 'y:', currentPosition.y, facing, 'obstacle :', statusObstacle);
 
     return (
-        <div>
+        <div className='container'>
             <h3>Instructions</h3>
-            <div>
+            <div className='buttons'>
+                <button onClick={rotateLeft} className='btnLeft'> Left </button>
+
                 <button onClick={goForward} className='btnForward'> Forward </button>
 
-                <button onClick={rotateLeft}> Left </button>
-
-                <button onClick={rotateRight}> Right </button>
+                <button onClick={rotateRight} className='btnRight'> Right </button>
             </div>
         </div>
     )
